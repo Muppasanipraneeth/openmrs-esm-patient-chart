55c2e5d50335069aaa6e815ef6462ca8a7409155
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-03-25 11:03:49 +0530

    feat: restructure growth chart configuration schema

--- packages/esm-patient-growth-chart-app/src/config-schema.ts
+++ packages/esm-patient-growth-chart-app/src/config-schema.ts
@@ -1,5 +1,48 @@
-import { immunizationWidgetSchema } from './growthchart/immunization-widget-config-schema';
-
 export const configSchema = {
-  immunizationsConfig: immunizationWidgetSchema,
+  concepts: {
+    weightUuid: {
+      _type: 'string',
+      _default: '5089AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    heightUuid: {
+      _type: 'string',
+      _default: '5090AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    bmiUuid: {
+      _type: 'string',
+      _default: '1342AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+    vitalSignsConceptSetUuid: {
+      _type: 'string',
+      _default: '160575AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+    },
+  },
+  biometrics: {
+    bmiUnit: {
+      _type: 'string',
+      _default: 'kg/m²',
+    },
+    weightUnit: {
+      _type: 'string',
+      _default: 'kg',
+    },
+    heightUnit: {
+      _type: 'string',
+      _default: 'Cm',
+    },
+  },
 };
+
+export interface BiometricsConfigObject {
+  bmiUnit: string;
+}
+
+export interface ConfigObject {
+  concepts: {
+    weightUuid: string;
+    heightUuid: string;
+    bmiUuid: string;
+    vitalSignsConceptSetUuid: string;
+  };
+  biometrics: BiometricsConfigObject;
+}
