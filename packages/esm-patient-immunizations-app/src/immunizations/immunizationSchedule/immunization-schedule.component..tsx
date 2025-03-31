2c11882979f13ae5f2336dcf8b924d8bc55fc3ad
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-03-31 14:21:30 +0530

    feat: Enhance immunization schedule component with pagination and improved vaccine display

--- packages/esm-patient-immunizations-app/src/immunizations/immunizationSchedule/immunization-schedule.component..tsx
+++ packages/esm-patient-immunizations-app/src/immunizations/immunizationSchedule/immunization-schedule.component..tsx
@@ -1,71 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useImmunizations } from '../../hooks/useImmunizations';
-import styles from './immunization-schedule.scss';
-import { Grid, Column, Tile, SkeletonText } from '@carbon/react';
-import { ErrorState } from '@openmrs/esm-framework';
-import ExpandableTileComponent from './expandable-tile-component';
+import { PatientChartPagination } from '@openmrs/esm-patient-common-lib';
+import { Table, TableHead, TableRow, TableHeader, TableBody, TableCell } from '@carbon/react';
+import { formatDate, parseDate } from '@openmrs/esm-framework';
 
-interface ImmunizationScheduleTileProps {
-  patientUuid: string;
-}
-
-const vaccineNameAbbreviations = {
-  'Bacillus Calmette–Guérin vaccine': 'BCG Vaccine',
-  'Polio vaccination, oral': 'OPV Vaccine',
-  'Diphtheria tetanus and pertussis vaccination': 'DTP Vaccine',
+const vaccineNames = {
+  'Bacillus Calmette–Guérin vaccine': 'BCG',
+  'Hemophilus influenza B vaccine': 'Hemophilus influenza B vaccine',
+  'Pentavalent pneumovax': 'Pentavalent pneumovax',
+  'Polio vaccine, inactivated': 'Polio vaccine, inactivated',
   'Vitamin A': 'Vitamin A',
+  'Polio vaccination, oral': 'Polio vaccination, oral',
+  'Diphtheria tetanus and pertussis vaccination': 'TdAP',
+  'Influenza vaccine': 'Flu',
+  'Hepatitis B vaccine': 'Hep B',
+  'Human Papillomavirus vaccine': 'HPV',
 };
 
-const ImmunizationSchedule: React.FC<ImmunizationScheduleTileProps> = ({ patientUuid }) => {
-  const { data: existingImmunizations, error, isLoading } = useImmunizations(patientUuid);
+const ImmunizationSchedule = ({ patientUuid }) => {
+  const { data: immunizations, error, isLoading } = useImmunizations(patientUuid);
 
-  if (isLoading) {
-    return <SkeletonText heading width="200px" className={styles.skeleton} />;
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 5;
 
-  if (error) {
-    return <ErrorState error={error} headerTitle="Immunization Schedule" />;
-  }
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading immunizations</div>;
+  if (!immunizations) return <div>No immunizations found</div>;
+
+  const sortedImmunizations = immunizations.map((immunization) => ({
+    ...immunization,
+    existingDoses: [...immunization.existingDoses].sort(
+      (a, b) => new Date(a.occurrenceDateTime).getTime() - new Date(b.occurrenceDateTime).getTime(),
+    ),
+  }));
+
+  let maxDoses = 1;
+  sortedImmunizations.forEach((immunization) => {
+    if (immunization.existingDoses.length > maxDoses) {
+      maxDoses = immunization.existingDoses.length;
+    }
+  });
+
+  const totalItems = sortedImmunizations.length;
+  const totalPages = Math.ceil(totalItems / pageSize);
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedImmunizations = sortedImmunizations.slice(startIndex, endIndex);
+
+  const formatDates = (date) => {
+    return formatDate(parseDate(date), { mode: 'standard', time: 'for today' });
+  };
+
+  const rows = paginatedImmunizations.map((immunization) => {
+    const vaccineName = vaccineNames[immunization.vaccineName] || immunization.vaccineName;
+
+    const row = {
+      id: immunization.vaccineUuid,
+      vaccine: { vaccineName },
+      doses: Array.from({ length: maxDoses }).map((_, index) => {
+        const dose = immunization.existingDoses[index];
+        return (
+          <span key={index}>
+            {dose ? (
+              <>
+                {' # ' + dose.doseNumber}
+                <br />
+                {formatDates(dose.occurrenceDateTime)}
+              </>
+            ) : (
+              <> </>
+            )}
+          </span>
+        );
+      }),
+    };
+    return row;
+  });
+
+  const headers = [
+    { key: 'vaccine', header: 'Vaccine' },
+    { key: 'doses', header: 'Doses' },
+  ];
+
+  const goTo = ({ page }) => {
+    setCurrentPage(page);
+  };
 
   return (
-    <div className={styles.immunizationSchedule}>
-      <Grid fullWidth narrow className={styles.immunizationGrid}>
-        <Column sm={4} md={8} lg={16} className={styles.container}>
-          <div className={styles.vaccineRows}>
-            {existingImmunizations?.length > 0
-              ? existingImmunizations.map((immunization, index) => {
-                  const sortedDoses = [...immunization.existingDoses].sort(
-                    (a, b) => new Date(a.occurrenceDateTime).getTime() - new Date(b.occurrenceDateTime).getTime(),
-                  );
-                  const displayVaccineName =
-                    vaccineNameAbbreviations[immunization.vaccineName] || immunization.vaccineName;
+    <div>
+      <Table size="xl" useZebraStyles={false} aria-label="Immunization Schedule">
+        <TableHead>
+          <TableRow style={{ backgroundColor: '#f4f4f4' }}>
+            {headers.map((header) => (
+              <TableHeader
+                key={header.key}
+                style={{
+                  minWidth: header.key === 'vaccine' ? '200px' : '80px',
+                  backgroundColor: '#f4f4f4',
+                }}
+                colSpan={header.key === 'doses' ? maxDoses : 1}
+              >
+                {header.header}
+              </TableHeader>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell style={{ backgroundColor: '#ffffff' }}>{row.vaccine.vaccineName}</TableCell>
+              {row.doses.map((dose, index) => (
+                <TableCell key={index} style={{ backgroundColor: '#ffffff' }}>
+                  {dose}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
-                  return (
-                    <div key={index} className={styles.vaccineRow}>
-                      <div className={styles.vaccineName}>
-                        <Tile className={styles.vaccineName}>{displayVaccineName}</Tile>
-                      </div>
-                      <div className={styles.vaccineItem}>
-                        {sortedDoses.map((dose, doseIndex) => (
-                          <div key={doseIndex} className={styles.doseItem}>
-                            <ExpandableTileComponent
-                              index={index}
-                              doseIndex={doseIndex}
-                              dose={{
-                                ...dose,
-                                vaccineName: immunization.vaccineName,
-                              }}
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  );
-                })
-              : null}
-          </div>
-        </Column>
-      </Grid>
+      <PatientChartPagination
+        totalItems={totalItems}
+        pageSize={pageSize}
+        pageNumber={currentPage}
+        currentItems={paginatedImmunizations.length}
+        onPageNumberChange={goTo}
+      />
     </div>
   );
 };
